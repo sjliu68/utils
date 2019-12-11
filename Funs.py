d2e26bb738eeb374e2c98d01b107459e43c85004
Shengjie Liu <liushengjie0756@gmail.com>
2019-12-11 18:04:41 +0800

    Create Funs.py

---
+++ Funs.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy.stats as st
+
+# Generate a Gaussian kernel 
+# rbf = gkern(5,2.5)*273
+def gkern(kernlen=21, nsig=3):
+    """Returns a 2D Gaussian kernel."""
+    x = np.linspace(-nsig, nsig, kernlen+1)
+    kern1d = np.diff(st.norm.cdf(x))
+    kern2d = np.outer(kern1d, kern1d)
+    return kern2d/kern2d.sum()
